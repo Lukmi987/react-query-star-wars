7715f613cbeb84637b76c9c5eeb4c7fb137c0aef
Lukas <>
2021-02-10 21:48:29 +0100

    react-query

---
+++ src/components/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Person = ( {...person} ) => {
+    console.log('my person', person);
+    return(
+    <div className="card">
+        <h3>{person.name}</h3>
+        <p>Birth year {person.birth_year}</p> */}
+    </div>
+    );
+}
+
+export default Person;
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useQuery, QueryClient } from 'react-query';
+import Planet from './Planet'
+import { useQuery } from 'react-query';
 
 const fetchPlanets = async () => {
     const res = await fetch('https://swapi.dev/api/planets/');
@@ -13,6 +14,23 @@ const Planets = () => {
     return (
         <div>
             <h2>Planets</h2>
+
+            {/* <p>{status}</p> */}
+
+            { status === 'loading' && (
+                <div>Loading fetching data</div>
+            )}
+
+
+                {status === 'error' && (
+                    <div>Error fetching data</div>
+                )}
+            {status === 'success' && (
+                <div>
+                    { data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+                </div>
+            )}
+
         </div>
     )
 }
